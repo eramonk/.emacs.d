e9a3691b206e81b7e9f0a695279c63367adcecc4
ra <eramonk@gmail.com>
2018-02-22 20:54:01 +0300

    debug

--- init.el
+++ init.el
@@ -41,7 +41,11 @@
 
 (global-set-key [C-down] (lambda () (interactive) (scroll-up 1)))
 (global-set-key [C-up] (lambda () (interactive) (scroll-down 1)))
-(global-set-key (kbd "<f2>") 'bs-show)
+;;Window resiz
+(global-set-key (kbd "C-c z <left>") 'shrink-window-horizontally)
+(global-set-key (kbd "C-c z <right>") 'enlarge-window-horizontally)
+(global-set-key (kbd "C-c z <down>") 'shrink-window)
+(global-set-key (kbd "C-c z <up>") 'enlarge-window)
 
 (use-package clojure-mode)
 (use-package 4clojure)
@@ -168,72 +172,7 @@ opening 4clojure questions"
 (setq bs-configurations
       '(("files" "^\\*scratch\\*" nil nil bs-visits-non-file bs-sort-buffer-interns-are-last)))
 
-
-
-;;Window resize
-
-(defun win-resize-top-or-bot ()
-  "Figure out if the current window is on top, bottom or in the
-middle"
-  (let* ((win-edges (window-edges))
-	 (this-window-y-min (nth 1 win-edges))
-	 (this-window-y-max (nth 3 win-edges))
-	 (fr-height (frame-height)))
-    (cond
-     ((eq 0 this-window-y-min) "top")
-     ((eq (- fr-height 1) this-window-y-max) "bot")
-     (t "mid"))))
-
-(defun win-resize-left-or-right ()
-  "Figure out if the current window is to the left, right or in the
-middle"
-  (let* ((win-edges (window-edges))
-	 (this-window-x-min (nth 0 win-edges))
-	 (this-window-x-max (nth 2 win-edges))
-	 (fr-width (frame-width)))
-    (cond
-     ((eq 0 this-window-x-min) "left")
-     ((eq (+ fr-width 4) this-window-x-max) "right")
-     (t "mid"))))
-
-(defun win-resize-enlarge-horiz ()
-  (interactive)
-  (cond
-   ((equal "top" (win-resize-top-or-bot)) (enlarge-window -1))
-   ((equal "bot" (win-resize-top-or-bot)) (enlarge-window 1))
-   ((equal "mid" (win-resize-top-or-bot)) (enlarge-window -1))
-   (t (message "nil"))))
-
-(defun win-resize-minimize-horiz ()
-  (interactive)
-  (cond
-   ((equal "top" (win-resize-top-or-bot)) (enlarge-window 1))
-   ((equal "bot" (win-resize-top-or-bot)) (enlarge-window -1))
-   ((equal "mid" (win-resize-top-or-bot)) (enlarge-window 1))
-   (t (message "nil"))))
-
-(defun win-resize-enlarge-vert ()
-  (interactive)
-  (cond
-   ((equal "left" (win-resize-left-or-right)) (enlarge-window-horizontally -1))
-   ((equal "right" (win-resize-left-or-right)) (enlarge-window-horizontally 1))
-   ((equal "mid" (win-resize-left-or-right)) (enlarge-window-horizontally -1))))
-
-(defun win-resize-minimize-vert ()
-  (interactive)
-  (cond
-   ((equal "left" (win-resize-left-or-right)) (enlarge-window-horizontally 1))
-   ((equal "right" (win-resize-left-or-right)) (enlarge-window-horizontally -1))
-   ((equal "mid" (win-resize-left-or-right)) (enlarge-window-horizontally 1))))
-
-(global-set-key [M-down] 'win-resize-minimize-vert)
-(global-set-key [M-up] 'win-resize-enlarge-vert)
-(global-set-key [M-left] 'win-resize-minimize-horiz)
-(global-set-key [M-right] 'win-resize-enlarge-horiz)
-(global-set-key [M-up] 'win-resize-enlarge-horiz)
-(global-set-key [M-down] 'win-resize-minimize-horiz)
-(global-set-key [M-left] 'win-resize-enlarge-vert)
-(global-set-key [M-right] 'win-resize-minimize-vert)
+(global-set-key (kbd "<f2>") 'bs-show)
 
 
 (custom-set-variables
